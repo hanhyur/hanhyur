3c704ab7e01cef391b6465479270ea5353ee1ae0
hanhyur <zeidgame@gmail.com>
2024-09-29 20:58:07 +0900

    feat : update readme
    <description>
    >> change : api fix
    
    <version>
    
    <issue>

--- build-svg.js
+++ build-svg.js
@@ -30,7 +30,7 @@ const todayDay = new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(ne
 // })
 
 // Today's weather
-weather.setLang("en");
+/*weather.setLang("en");
 weather.setCoordinate(37.517235, 127.047325);
 weather.setUnits("imperial");
 weather.setAPPID(WEATHER_API_KEY);
@@ -41,6 +41,36 @@ weather.getWeatherOneCall(function(err, data) {
   const degF = Math.round(data.daily[0].temp.max);
   const degC = Math.round(qty(`${degF} tempF`).to("tempC").scalar);
   const icon = data.daily[0].weather[0].icon;
+
+  fs.readFile("template.svg", "utf-8", (error, data) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    data = data.replace("{degF}", degF);
+    data = data.replace("{degC}", degC);
+    data = data.replace("{weatherEmoji}", emojis[icon]);
+    // data = data.replace('{psTime}', psTime)
+    data = data.replace("{todayDay}", todayDay);
+
+    data = fs.writeFile("chat.svg", data, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    });
+  });
+});*/
+
+weather.setAPPID(WEATHER_API_KEY);
+
+weather.getWeatherCity(function(err, data) {
+  if (err) console.log(err);
+
+  const degC = Math.round(data.main.temp_max);
+  const degF = Math.round(qty(`${degC} tempC`).to("tempF").scalar);
+  const icon = data.weather.icon;
 
   fs.readFile("template.svg", "utf-8", (error, data) => {
     if (error) {
